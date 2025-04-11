5c06ac7e3c6b0a42680f97a152855f004ea976cf
manasvpn30 <155732075+manasvpn30@users.noreply.github.com>
2025-04-11 22:59:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+//console.log(`Manasvi`);
+console.log(`i like icecream`);
+
+window.alert(`This is an alert!`);
+window.alert(`Dinosaraus are alive!`);
+window.alert(`stay away from nivedaaa!`);
+document.getElementById("mpn1").textContent=`Manasvi`;
+document.getElementById("myp").textContent="Mohitha is looking at her phone";
